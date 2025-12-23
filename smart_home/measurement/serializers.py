a009755a287c2e37027746930db23ff4908c39ee
Vladislav <vladislav.goltsev@gmail.com>
2025-12-23 16:53:44 +0300

    create superuser and, class Measurement,

--- smart_home/measurement/serializers.py
+++ smart_home/measurement/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 
-from .models import Sensor
+from .models import Sensor, Measurement
 
-
-# TODO: опишите необходимые сериализаторы
 
 class SensorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sensor
         fields = ['id', 'name', 'description']
 
-
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['sensor_id', 'temperature', 'created_at']
